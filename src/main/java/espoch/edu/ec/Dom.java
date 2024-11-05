12b938a31da5ca1a13f37bce643ecb6991fbd629
ShellDomenic <domenicaloor177@gmail.com>
2024-11-05 09:35:40 -0500

    Hello people

---
+++ src/main/java/espoch/edu/ec/Dom.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espoch.edu.ec;
+
+/**
+ *
+ * @author and_j
+ */
+public class Dom {
+
+    public static void main(String[] args) {
+        System.out.println("Hello people");
+    }
+}
